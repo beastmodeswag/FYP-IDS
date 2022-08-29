98192fe26db1ae315b7617e9aa8056c1c7c76d0b
biter3 <106904493+biter3@users.noreply.github.com>
2022-08-29 02:16:02 -0400

    Add files via upload

--- packetSniffer.py
+++ packetSniffer.py
@@ -106,7 +106,13 @@ def main():
 			# TCP
 			elif proto == 6:
 				(src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data) = tcp_segment(data)
-			
+				print(TAB_1 + 'TCP Segment:')
+				print(TAB_2 + 'Source Port: {}, Destination Port: {}'.format(src_port, dest_port))
+				print(TAB_2 + 'Sequence: {}, Acknowledgement: {}'.format(sequence, acknowledgement))
+				print(TAB_2 + 'Flags:')
+				print(TAB_3 + 'URG: {}, ACK: {}, PSH: {}, RST: {}, SYN: {}, FIN: {}'.format(flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin))
+				print(TAB_2 + 'Data:')
+				print(format_multi_line(DATA_TAB_3, data))
 			#UDP
 			elif proto == 17:
 				src_port, dest_port, length, data = udp_segment(data)
